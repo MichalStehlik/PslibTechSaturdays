49664bc0e3ebcd7eac84497ea44a51d433f9c6e8
Michal Stehlík <michal.stehlik@pslib.cz>
2023-10-16 22:41:53 +0200

    Group Detail screen

---
+++ PslibTechSaturdays/Pages/Group.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PslibTechSaturdays.Data;
+using PslibTechSaturdays.Models;
+
+namespace PslibTechSaturdays.Pages
+{
+    public class GroupModel : PageModel
+    {
+        private readonly PslibTechSaturdays.Data.ApplicationDbContext _context;
+        private readonly ILogger<GroupModel> _logger;
+
+        public GroupModel(ApplicationDbContext context, ILogger<GroupModel> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public Group Group { get; set; }
+
+        [TempData]
+        public string? SuccessMessage { get; set; }
+        [TempData]
+        public string? FailureMessage { get; set; }
+        public async Task OnGetAsync(int id)
+        {
+            Group = await _context.Groups!.FirstOrDefaultAsync(g => g.GroupId == id);
+            _context.Entry(Group).Collection(p => p.Tags!).Load();
+            _context.Entry(Group).Collection(p => p.Lectors!).Load();
+            _context.Entry(Group).Collection(p => p.Enrollments!).Load();
+            _context.Entry(Group).Reference(p => p.Action!).Load();
+        }
+    }
+}
